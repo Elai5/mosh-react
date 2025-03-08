88111524d898bd46f13f0c01c6b9b7b8cc8b678f
Elai5 <yvettelaine007@gmail.com>
2025-03-08 13:58:09 +0300

    removed bootstraplist added manual list

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,16 +1,25 @@
 /** @format */
+// import { Fragment } from "react/jsx-runtime";
 
 function ListGroup() {
+  const items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+  items.map((item) => <li>{item}</li>);
   return (
-    <ul className="list-group">
-      <li className="list-group-item active" aria-current="true">
-        An active item
-      </li>
-      <li className="list-group-item">A second item</li>
-      <li className="list-group-item">A third item</li>
-      <li className="list-group-item">A fourth item</li>
-      <li className="list-group-item">And a fifth one</li>
-    </ul>
+    <>
+      <h1>List Item</h1>
+      <ul className="list-group">
+        {items.map((item) => (
+          <li>{item}</li>
+        ))}
+        {/* <li className="list-group-item active" aria-current="true">
+          An active item
+        </li>
+        <li className="list-group-item">A second item</li>
+        <li className="list-group-item">A third item</li>
+        <li className="list-group-item">A fourth item</li>
+        <li className="list-group-item">And a fifth one</li> */}
+      </ul>
+    </>
   );
 }
 export default ListGroup;
