8a659663f6e498559f5da1d339a38698c420d72c
Elai5 <yvettelaine007@gmail.com>
2025-03-08 16:46:58 +0300

    event hadler

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -3,13 +3,24 @@
 
 function ListGroup() {
   let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
-  items = [];
+  // items = [];
 
   return (
     <>
       <h1>List Item</h1>
       {/* {items.length === 0 ? <p>no item found</p> : null} */}
       {items.length === 0 && <p>no item found</p>}
+      <ul className="list-group">
+        {items.map((item) => (
+          <li
+            className="list-group-item"
+            key={item}
+            onClick={() => console.log("Clicked")}
+          >
+            {item}
+          </li>
+        ))}
+      </ul>
     </>
   );
   // if (items.length === 0)
@@ -19,24 +30,5 @@ function ListGroup() {
   //       <p>No item found</p>
   //     </>
   //   );
-
-  items.map((item) => <li>{item}</li>);
-  return (
-    <>
-      <h1>List Item</h1>
-      <ul className="list-group">
-        {items.map((item) => (
-          <li key={item}>{item}</li>
-        ))}
-        {/* <li className="list-group-item active" aria-current="true">
-          An active item
-        </li>
-        <li className="list-group-item">A second item</li>
-        <li className="list-group-item">A third item</li>
-        <li className="list-group-item">A fourth item</li>
-        <li className="list-group-item">And a fifth one</li> */}
-      </ul>
-    </>
-  );
 }
 export default ListGroup;
